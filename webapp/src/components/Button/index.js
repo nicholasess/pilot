9e4aee876a526ef4e3e073b412a12cf71d6fd7ef
Lucas Duarte <lc.sales.duarte@gmail.com>
2017-08-17 19:18:19 -0300

    button: add degrade, flat, outline and dashed

--- webapp/src/components/Button/index.js
+++ webapp/src/components/Button/index.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
 import style from './style.css'
 
-const Button = ({ children }) => (
-  <button className={style.button}>
-    {children}
-  </button>
-)
+function Button ({ type, color, children }) {
+  const className = classNames(
+    style.button,
+    style[color],
+    style[type]
+  )
+
+  return (
+    <button className={className}>
+      {children}
+    </button>
+  )
+}
 
 Button.propTypes = {
   children: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
 }
 
 export default Button
--- webapp/stories/Button/index.js
+++ webapp/stories/Button/index.js
@@ -6,7 +6,15 @@ import { action } from '@storybook/addon-actions'
 import Button from '../../src/components/Button'
 import style from './style.css'
 
-storiesOf('Button', module)
-  .add('primary', () =>
-    <Button onClick={action('clicked')}>Primary Button</Button>)
+storiesOf( 'Button', module )
+  .add( 'green flat', () =>
+    <Button onClick={action( 'clicked' )} color="green" type="flat">Flat Button</Button> )
 
+  .add( 'green degrade', () =>
+    <Button onClick={action( 'clicked' )} color="green" type="degrade">Degrade Button</Button> )
+
+  .add( 'green outline', () =>
+    <Button onClick={action( 'clicked' )} color="green" type="outline">Outline Button</Button> )
+
+  .add( 'green dashed', () =>
+    <Button onClick={action( 'clicked' )} color="green" type="dashed">Dashed Button</Button> )
