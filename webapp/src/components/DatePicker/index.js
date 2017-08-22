c2293e2cf0ea20d788dd0bb020cbb43ccb9ded5c
Felquis Gimenes <frgformenton@gmail.com>
2017-08-22 04:04:05 -0300

    wip

--- webapp/src/components/DatePicker/index.js
+++ webapp/src/components/DatePicker/index.js
@@ -8,8 +8,32 @@ import {
   merge,
   omit,
 } from 'ramda'
+import classNames from 'classnames'
+import IconAngleLeft from 'react-icons/lib/fa/angle-left'
+import IconAngleRight from 'react-icons/lib/fa/angle-right'
+import tz from 'moment-timezone'
 
 import './style.scss'
+
+moment.updateLocale('pt-BR', {
+  weekdaysMin: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+  months: [
+    'Janeiro',
+    'Feveiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro'
+  ],
+})
+
+moment.locale('pt-BR')
 
 class DatePicker extends Component {
   constructor (props) {
@@ -57,7 +81,10 @@ class DatePicker extends Component {
 
   render () {
     const defaultProps = {
-      hideKeyboardShortcutsPanel: true
+      hideKeyboardShortcutsPanel: true,
+      navPrev: <IconAngleLeft />,
+      navNext: <IconAngleRight />,
+      keepOpenOnDateSelect: true
     }
 
     const {
@@ -65,7 +92,7 @@ class DatePicker extends Component {
       endDate,
       focusedInput,
       date,
-      focused
+      focused,
     } = this.state
 
     const {
@@ -77,26 +104,65 @@ class DatePicker extends Component {
       this.props
     )
 
+    const className = classNames({
+      'DatePicker': true,
+      'DatePicker_default': !range,
+      'DatePicker_range': range,
+      'DatePicker_open': focused || focusedInput
+    })
+
     return (
       <div>
         {(range? (
-          <DateRangePicker
-            {...merge(defaultProps, filteredProps)}
-            startDate={startDate}
-            endDate={endDate}
-            focusedInput={focusedInput}
-            onDatesChange={this.onDatesChange}
-            onFocusChange={this.onRangeFocusChange}
-          />
+          <div
+            className={className}
+          >
+            <div>
+              Uns treco!
+            </div>
+
+            <DateRangePicker
+              {...merge(defaultProps, filteredProps)}
+              startDate={startDate}
+              endDate={endDate}
+              focusedInput={focusedInput}
+              onDatesChange={this.onDatesChange}
+              onFocusChange={this.onRangeFocusChange}
+            />
+
+            <div
+              className="DatePicker_confirmation">
+              <button
+                className="DatePicker_confirmation__button"
+              >Cancelar</button>
+              <button
+                className="DatePicker_confirmation__button"
+              >Ok</button>
+            </div>
+          </div>
         ) : (
-          <SingleDatePicker
-            {...merge(defaultProps, filteredProps)}
-            focused={focused}
-            date={date}
-            numberOfMonths={1}
-            onDateChange={this.onDateChange}
-            onFocusChange={this.onFocusChange}
-          />
+          <div
+            className={className}
+          >
+            <SingleDatePicker
+              {...merge(defaultProps, filteredProps)}
+              focused={focused}
+              date={date}
+              numberOfMonths={1}
+              onDateChange={this.onDateChange}
+              onFocusChange={this.onFocusChange}
+            />
+
+            <div
+              className="DatePicker_confirmation">
+              <button
+                className="DatePicker_confirmation__button"
+              >Cancelar</button>
+              <button
+                className="DatePicker_confirmation__button"
+              >Ok</button>
+            </div>
+          </div>
         )
       )}
       </div>
