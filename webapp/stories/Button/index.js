4294d5230bd087f13b31dd8c8e0b10243d1f13c6
Lucas Duarte <lc.sales.duarte@gmail.com>
2017-08-23 19:04:25 -0300

    button: move component to separated path
    Separate componente to use style.css

--- webapp/stories/Button/index.js
+++ webapp/stories/Button/index.js
@@ -3,7 +3,8 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 
-import Button from '../src/components/Button'
+import Button from '../../src/components/Button'
+import style from './style.css'
 
 storiesOf('Button', module)
   .add('primary', () =>
